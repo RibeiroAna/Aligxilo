c9d1878d55b49139d87590d67e8a21a373964d2d
Jefferson Benedito <jefiim.ad@gmail.com>
2017-09-26 06:31:10 -0300

    Aldonita route file

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
 var app = angular.module("formularoj", ["ui.router", "ui.mask", "xeditable"])
 
-app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/form/prihomo");
-
-        $stateProvider
-            .state("form", {
-                url: "/form",
-                templateUrl: "template/form.htm"
-            })
-            .state("form.prihomo", {
-                url: "/prihomo",
-                templateUrl: "template/prihomo.htm",
-                controller: "prihomoCtrl"
-            })
-            .state("form.membrigxi", {
-                url: "/membrigxi",
-                templateUrl: "template/membrigxi.htm",
-                controller: "membrigxiCtrl"
-            })
-            .state("form.donaci", {
-                url: "/donaci",
-                templateUrl: "template/donaci.htm",
-                controller: "donaciCtrl"
-            })
-            .state("form.plusendi", {
-                url: "/plusendi",
-                templateUrl: "template/plusendi.htm",
-                controller: "plusendiCtrl"
-            })
-    });
-
 app.run(function(editableOptions) {
   editableOptions.theme = 'bs3';
 });
